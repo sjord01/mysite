f11c08a26d8b4102c4e4203a584b589e529b834b
Sam Ordonez <samordonez.ce@gmail.com>
2024-01-08 10:33:23 -0800

    removed hardcoding elements and replaced with dynamic python codes

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.http import Http404
-from django.shortcuts import render
+
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 
@@ -14,12 +14,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
-
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
